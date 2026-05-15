9d20590d978ec374e9abed059b8f10903463a527
Thomas Zermeno <madcarrots@gmail.com>
2026-05-15 08:59:22 -0500

    First commit #100daysofcode

---
+++ week1/day01/day01.py
@@ -0,0 +1,15 @@
+print('Hello, Thomas')
+print("Are we not men?")
+print("No!")
+print("We are DEVO!")
+print("She said \n\"Baby, Python is my thing,\" \nand she was right.")
+
+# this is a comment
+print("This will be displayed when the program is executed.")
+# print("This will not be displayed.")
+# notice the color difference.
+
+# mini assignment 01
+print("Name: \tThomas Zermeno")
+print("Age: \t51")
+print("Hobby: \tAll things zombie")
