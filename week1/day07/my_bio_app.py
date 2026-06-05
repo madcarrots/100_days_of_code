8d4c1a958e18cbdbad23758a62097ae03d488862
Thomas Zermeno <Tom@SSL.com>
2026-06-05 16:16:06 -0500

    day 07 still working hastag tortoise code

--- week1/day07/my_bio_app.py
+++ week1/day07/my_bio_app.py
@@ -5,16 +5,26 @@
 
 # Welcome customer
 print("Welcome Customer!\nPlease provide us with some basic information about yourself.")
-name_str = input("What is your name? : \t")
-age_int = int(input("How old are you? : \t"))
-city_str = input("What city do you live in? : ")
+name_str = input("What is your name? : \t\t")
+age_int = int(input("How old are you? : \t\t"))
+city_str = input("What city do you live in? : \t")
 fave_lang = input("What is your favorite programming language? : ")
 hobby_input = input("Enter your 3 favorite hobbies, separated by a comma: ")
 hobby_list = hobby_input.split(",")
 hobbies = []
-hobby1 = input("What is your favorite hobby? : \t")
+hobby1 = input("What is your favorite hobby? : \t\t")
 hobbies.append(hobby1)
 hobby2 = input("What other hobby do you enjoy? : \t")
-hobby3 = input("What's one more hobby that you have? : ")
-hobbies.extend(hobby2, hobby3)
+hobby3 = input("What's one more hobby that you have? : \t")
+hobbies.append(hobby2)
+hobbies.append(hobby3)
 
+print("\n\n********** Info Card **********")
+print(f" Name: {name_str} ")
+print(f" Age: \t{age_int}")
+print(f" Metro: {city_str}")
+
+print("\n Interests")
+print(f" 1) \t\t{hobbies[0]}")
+print(f" 2) \t\t{hobbies[1]}")
+print(f" 3) \t\t{hobbies[2]}")
